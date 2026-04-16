d706e59bc2fc08f10210f02745c9538a42f44b0f
Deepjyoti <deepjyoti692@gmail.com>
2026-04-17 01:23:43 +0530

    Requirements and installation done

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__="0.0.0"
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "Deepjyoti-hub"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "deepjyoti692@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for Text Summarization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    packages=setuptools.find_packages(where="src"),
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    python_requires=">=3.9"
+)
